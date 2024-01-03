17e515d5cd1305ec8bd117309dddf97e5bab31b0
unhappiness404 <153148643+unhappiness404@users.noreply.github.com>
2024-01-03 09:37:24 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240103/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i=0; i<n; i++) {
+            for(int j=n+1-i*2; j>0; j--) {
+                System.out.print(" ");
+            }
+
+            for(int j=0; j<=i; j++) {
+                System.out.print("@ ");
+            }
+            System.out.println();
+        }
+
+        for(int i=n-2; i>=0; i--) {
+            for(int j=0; j<=i; j++) {
+                System.out.print("@ ");
+            }
+
+            for(int j=n+1-i*2; j>0; j--) {
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+// n=3
+// 0 4 1 
+// 1 2 2
+// 2 0 3
+
+//3+1 -i*2
+
+// 
+// i*2+1
