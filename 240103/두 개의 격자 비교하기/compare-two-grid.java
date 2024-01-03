5a223b5720eba946317420eb8225664d04a738d9
unhappiness404 <153148643+unhappiness404@users.noreply.github.com>
2024-01-03 10:07:17 +0900

    Title: 두 개의 격자 비교하기, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240103/두 개의 격자 비교하기/compare-two-grid.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] arr = new int[n][m];
+        int[][] brr = new int[n][m];
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                brr[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(arr[i][j] == brr[i][j]) {
+                    System.out.print(0);
+                } else {
+                    System.out.print(1);
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
