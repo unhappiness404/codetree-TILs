3d3f1016d4c847a512ef0a02fd1b974aaa51dd31
unhappiness404 <153148643+unhappiness404@users.noreply.github.com>
2024-01-04 23:31:22 +0900

    Title: 배열로 사각형 만들기, Time: 83ms, Memory: 8MB, Status: Passed - CodeTree

--- 240104/배열로 사각형 만들기/print-array-in-rectangle.java
+++ 240104/배열로 사각형 만들기/print-array-in-rectangle.java
@@ -4,10 +4,9 @@ public class Main {
 
         int[][] arr = new int[n][n];
 
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                arr[i][j] = 1;
-            }
+        for(int i = 0; i < 5; i++) {
+            arr[i][0] = 1;
+            arr[0][i] = 1;
         }
 
         for(int i=1; i<n; i++) {
