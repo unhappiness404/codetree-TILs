a2c2c78da4a28a4aac623491a25a5fb376271d88
unhappiness404 <153148643+unhappiness404@users.noreply.github.com>
2024-01-30 16:44:12 +0900

    Title: 최대로 겹치는 지점, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240130/최대로 겹치는 지점/maximum-overlapped-points.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[100];
+
+        for(int i=0; i<n; i++) {
+            int x = sc.nextInt()-1;
+            int y = sc.nextInt()-1;
+            for(int j=x; j<y; j++) {
+                arr[j] += 1;
+            }
+        }
+        int max = ;
+        for(int i=0; i<100; i++) {
+            max = Math.max(arr[i], max);
+        }
+        System.out.print(max+1);
+    }
+}
