bb861176207d271a2107692708570b5cd9861271
unhappiness404 <153148643+unhappiness404@users.noreply.github.com>
2024-03-15 12:00:52 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240315/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        for(int i=0; i<5; i++) {
+            print10Stars();
+        }
+    }
+
+    public static void print10Stars() {
+        System.out.println("**********");
+    }
+}
