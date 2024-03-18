e0aaff9db36d1c7768bfdefb003416b4e9ba76ac
unhappiness404 <153148643+unhappiness404@users.noreply.github.com>
2024-03-18 16:47:57 +0900

    Title: Run Length 인코딩, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240318/Run Length 인코딩/run-length-encoding.java
+++ 240318/Run Length 인코딩/run-length-encoding.java
@@ -11,6 +11,7 @@ public class Main {
         for(int i = 1; i < str.length(); i++) {
             if( i == str.length() - 1) {
                 answer+=str.charAt(i)+String.valueOf(cnt+1);
+                break;
             }
             if(str.charAt(i-1) != str.charAt(i)) {
                 answer+=str.charAt(i-1)+String.valueOf(cnt);
