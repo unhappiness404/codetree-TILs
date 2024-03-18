9d2522268a75f3f48b62b3ad1beb9cb3cbfbf3ec
unhappiness404 <153148643+unhappiness404@users.noreply.github.com>
2024-03-18 17:29:59 +0900

    Title: Run Length 인코딩, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240318/Run Length 인코딩/run-length-encoding.java
+++ 240318/Run Length 인코딩/run-length-encoding.java
@@ -5,32 +5,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         String str = sc.next();
+
         String zip = "";
+
         int cnt = 1;
 
-        if(str.length() == 1) {
-            System.out.println(2);
-            System.out.println(str+1);
-        } else {
-            for(int i = 1; i < str.length(); i++) {
-                // 이전 문자와 현재 문자가 다르면
-                if(str.charAt(i-1) != str.charAt(i)) {
-                    zip += str.charAt(i-1) + String.valueOf(cnt);
-                    cnt = 0;
-                }
-                if(i == str.length() - 1) {
-                    zip += str.charAt(i) + String.valueOf(cnt+1);
-
-                }
-                cnt++;
+        for(int i = 1; i < str.length(); i++) {
+            if(str.charAt(i-1) != str.charAt(i)) {
+                zip += str.charAt(i-1) + String.valueOf(cnt);
+                cnt = 0;
             }
-            System.out.println(zip.length());
-            System.out.println(zip);
+            cnt++;
         }
+        zip += str.charAt(str.length()-1) + String.valueOf(cnt);
 
-
-
-
-
+        System.out.println(zip.length());
+        System.out.println(zip);
+        
     }
 }
