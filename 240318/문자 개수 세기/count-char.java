bb02c020d4d8e01b60d74752a23a205c5910ef09
unhappiness404 <153148643+unhappiness404@users.noreply.github.com>
2024-03-18 14:33:28 +0900

    Title: 문자 개수 세기, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240318/문자 개수 세기/count-char.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        char c = sc.next().charAt(0);
+
+        int cnt = 0;
+        for(int i=0; i<str.length(); i++) {
+            if(str.charAt(i) == c) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
