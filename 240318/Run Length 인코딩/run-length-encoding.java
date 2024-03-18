cbf27db946f5c55c14e7a35ad174c41efe3cc03d
unhappiness404 <153148643+unhappiness404@users.noreply.github.com>
2024-03-18 17:18:49 +0900

    Title: Run Length 인코딩, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240318/Run Length 인코딩/run-length-encoding.java
+++ 240318/Run Length 인코딩/run-length-encoding.java
@@ -5,25 +5,32 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         String str = sc.next();
+        String zip = "";
         int cnt = 1;
-        String answer = "";
 
-        for(int i = 1; i < str.length(); i++) {
-            if(str.length() == 1) {
-                answer+=str.charAt(0)+String.valueOf(1);
-                break;
-            }
-            if(str.charAt(i-1) != str.charAt(i)) {
-                answer+=str.charAt(i-1)+String.valueOf(cnt);
-                cnt = 0;
-            }
-            if( i == str.length() - 1) {
-                answer+=str.charAt(i)+String.valueOf(cnt+1);
-            }
+        if(str.length() == 1) {
+            System.out.println(1);
+            System.out.println(str+1);
+        } else {
+            for(int i = 1; i < str.length(); i++) {
+                // 이전 문자와 현재 문자가 다르면
+                if(str.charAt(i-1) != str.charAt(i)) {
+                    zip += str.charAt(i-1) + String.valueOf(cnt);
+                    cnt = 0;
+                }
+                if(i == str.length() - 1) {
+                    zip += str.charAt(i) + String.valueOf(cnt+1);
 
-            cnt++;
+                }
+                cnt++;
+            }
+            System.out.println(zip.length());
+            System.out.println(zip);
         }
-        System.out.println(answer.length());
-        System.out.println(answer);
+
+
+
+
+
     }
 }
