91a228375ef198df78e4ae38a97beb3cf368cf54
unhappiness404 <153148643+unhappiness404@users.noreply.github.com>
2024-03-27 23:51:26 +0900

    Title: 재귀함수의 꽃, Time: 164ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240327/재귀함수의 꽃/the-flower-of-the-recursive-function.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        printNumbers(n);
+    }
+
+    public static void printNumbers(int n) {
+        if(n == 0) {
+            return;
+        }
+
+        System.out.print(n+" ");
+
+        printNumbers(n - 1);
+
+        System.out.print(n+" ");
+    }
+}
